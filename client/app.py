1979bcda38c1e41e5ace497aeb8e9f5d92b667ec
yaramt <mataniyara@gmail.com>
2025-09-26 20:22:40 +0300

    Specify path to static and templates

--- client/app.py
+++ client/app.py
@@ -21,7 +21,7 @@ from .src.tools import allowed_file, input2dnavi, move_dnavi_files
 # CONFIGURE APP
 ###############################################################################
 login_manager = LoginManager()
-base_dir = os.path.dirname(os.path.abspath(__file__))  # points to 'client'
+base_dir = os.path.dirname(os.path.abspath(__file__))
 template_dir = os.path.join(base_dir, 'templates')
 static_dir = os.path.join(base_dir, 'static')
 
